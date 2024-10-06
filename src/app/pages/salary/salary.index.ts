3899bc546d096bebd4b9556af2f898532ed54102
nischalbhandari12 <nischal.bhandari@mentorfriends.com>
2024-10-06 12:50:44 +0545

    added nischal changes

--- src/app/pages/salary/salary.index.ts
+++ src/app/pages/salary/salary.index.ts
@@ -1,4 +1,4 @@
-import { Concept, ConnectionData, CreateConnectionBetweenTwoConceptsLocal, CreateTheConnection, CreateTheConnectionLocal, FormatFromConnectionsAltered, GetCompositionFromConnectionsWithDataIdInObject, GetConnectionBulk, GetConnectionDataPrefetch, GetTheConcept, LocalSyncData, MakeTheInstanceConcept, MakeTheInstanceConceptLocal, MakeTheTypeConcept, PatcherStructure, SearchLinkMultipleAll, SearchLinkMultipleApi, SearchQuery, SyncData, UpdateCompositionLocal } from "mftsccs-browser";
+import { ConnectionData, CreateConnectionBetweenTwoConceptsLocal, CreateTheConnection, CreateTheConnectionLocal, FormatFromConnectionsAltered, GetCompositionFromConnectionsWithDataIdInObject, GetConnectionBulk, GetConnectionDataPrefetch, GetTheConcept, LocalSyncData, MakeTheInstanceConcept, MakeTheInstanceConceptLocal, MakeTheTypeConcept, PatcherStructure, SearchLinkMultipleAll, SearchLinkMultipleAllWidget, SearchLinkMultipleApi, SearchQuery, SyncData, UpdateCompositionLocal } from "mftsccs-browser";
 import { StatefulWidget } from "../../default/StatefulWidget";
 
 
@@ -41,54 +41,49 @@ export class Salary extends StatefulWidget{
 
 
 
-    listenToEvent(id: number) {
-        window.addEventListener(`${id}`, (event) => {
-            if(!this.isUpdating){
-                this.isUpdating = true;
-                console.log("this is the updating method", id);
-                let that = this;
-                console.log("This is the old connections", that.internalConnections,that.compositionIds);
+    // listenToEvent(id: number) {
+    //     window.addEventListener(`${id}`, (event) => {
+    //         if(!this.isUpdating){
+    //             this.isUpdating = true;
+    //             let that = this;
 
-                setTimeout(function(){
-                    let newConnection = ConnectionData.GetConnectionByOfTheConceptAndType(id, id);
-                    for(let i=0 ;i< newConnection.length; i++){
+    //             setTimeout(function(){
+    //                 let newConnection = ConnectionData.GetConnectionByOfTheConceptAndType(id, id);
+    //                 for(let i=0 ;i< newConnection.length; i++){
                         
-                                ConnectionData.GetConnection(newConnection[i]).then((conn)=>{
-                                    if(conn.typeId == that.mainConcept){
-                                        if(!that.internalConnections.includes(conn.id)){
-                                            that.internalConnections.push(conn.id);
-                                        }
-                                    }
-                                    else{
-                                        if(!that.linkers.includes(conn.id)){
-                                            that.linkers.push(conn.id);
-                                        console.log("this is updating the linker", that.linkers);
+    //                             ConnectionData.GetConnection(newConnection[i]).then((conn)=>{
+    //                                 if(conn.typeId == that.mainConcept){
+    //                                     if(!that.internalConnections.includes(conn.id)){
+    //                                         that.internalConnections.push(conn.id);
+    //                                     }
+    //                                 }
+    //                                 else{
+    //                                     if(!that.linkers.includes(conn.id)){
+    //                                         that.linkers.push(conn.id);
 
-                                        }
-                                    }
+    //                                     }
+    //                                 }
 
-                                    if(!that.compositionIds.includes(conn.toTheConceptId)){
-                                        that.compositionIds.push(conn.toTheConceptId);
-                                        console.log("This is the updated connections", that.internalConnections, that.compositionIds, that.linkers);
-                                         that.isUpdating = false;
-                                        console.log("now again this can work");
+    //                                 if(!that.compositionIds.includes(conn.toTheConceptId)){
+    //                                     that.compositionIds.push(conn.toTheConceptId);
+    //                                      that.isUpdating = false;
     
-                                    }
-                                    that.setState();
+    //                                 }
+    //                                 that.setState();
 
-                                });
+    //                             });
 
-                    }
+    //                 }
 
 
-                }, 1000);
-            }
-            else{
-                console.log("rejected this");
-            }
+    //             }, 1000);
+    //         }
+    //         else{
+    //             console.log("rejected this");
+    //         }
 
-        });
-    }
+    //     });
+    // }
 
     addEvents(){
         let myIncreaseButton = document.getElementById("increase-count");
@@ -128,61 +123,22 @@ export class Salary extends StatefulWidget{
 
         let searchQuery2 = new SearchQuery();
         searchQuery2.listLinkers = ["read_access_by"];
+        let widget = new SearchLinkMultipleAllWidget([searchQuery, searchQuery2]).onchange();
 
-        this.mainData =  await SearchLinkMultipleAll([searchQuery, searchQuery2], "", this);
+       // this.mainData =  await SearchLinkMultipleAll([searchQuery, searchQuery2], "", this);
         console.log("this is the main data", this.mainData);
-        // let conceptsConnections = await SearchLinkMultipleApi([searchQuery, searchQuery2]);
-        // this.mainConcept = searchQuery.composition;
-        // const result = conceptsConnections;
-        // this.concepts = result.compositionIds;
-        // this.internalConnections = result.internalConnections;
-        // this.connections = result.linkers;
-        // this.reverse = result.reverse;
-        // if(this.internalConnections.length > 1){
-        //     console.log("this is the con", this.internalConnections);
-        //     let allConnections = await GetConnectionBulk(this.internalConnections);
-        //     for(let i=0; i< allConnections.length; i++){
-        //         if(!this.dependency.includes(allConnections[i].ofTheConceptId)){
-        //             this.dependency.push(allConnections[i].ofTheConceptId);
-
-        //         }
-        //         if(!this.dependency.includes(allConnections[i].toTheConceptId)){
-        //             this.dependency.push(allConnections[i].toTheConceptId);
-
-        //         }
-        //     }
-        // }
-
-
-
-        // const [prefetchConnections, concepts] = await Promise.all([
-        //   GetConnectionDataPrefetch(linkers),
-        //   GetCompositionFromConnectionsWithDataIdInObject(conceptIds,connections)
-        // ]); 
-        this.listenToEvent(this.mainConcept);
+        //this.listenToEvent(this.mainConcept);
 
 
     }
 
-    async buildData(){
-        // let externalConnections = this.connections.slice();
-        // let internalConn = this.internalConnections.slice();
-        // let conceptIds = this.concepts.slice();
-        // let prefetchConnections = await GetConnectionDataPrefetch(externalConnections);
 
-        // let concepts = await GetCompositionFromConnectionsWithDataIdInObject(conceptIds, internalConn);
-        
-        // let out = await FormatFromConnectionsAltered(prefetchConnections, concepts, this.mainConcept, this.reverse);
-        // console.log("this is the data", out);
-        // this.mainData = out;
-    }
 
 
 
 
     async setState(){
         await this.setDataDependency();
-        //await this.buildData();
         this.render();
     }
 
